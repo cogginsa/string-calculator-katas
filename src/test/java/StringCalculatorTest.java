e7bbbd0f1adfb1fd0e2ff617f488d70f82bc754b
Abbie Coggins <abbie.coggins@reedbusiness.com>
2020-06-24 13:57:02 +0100

    A single number returns the value

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,14 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
 
     @Test
     public void testEmptyReturnsZero() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         String result = stringCalculator.add("");
         assertEquals("0", result);
+    }
+
+
+    @Test
+    public void testSingleInputReturnsSameValue() {
+        String result = stringCalculator.add("3");
+        assertEquals("3", result);
     }
 
 }
