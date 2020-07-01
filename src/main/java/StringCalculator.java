7933f0a85144d1115cbc74b642f4e11a9ec39ba1
Abbie Coggins <abbie.coggins@reedbusiness.com>
2020-07-01 15:11:55 +0100

    reset to base level

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,35 +1,6 @@
 public class StringCalculator{
 
     public String add(String str){
-        int sum = 0;
-        try {
-            String[] validatedArray = validateInput(str.split(",|\\\n"));
-
-            for (String value : validatedArray) {
-                if(!value.isEmpty()){
-                    sum += Integer.parseInt(value);
-                }
-            }
-        } catch (NegativeNumberException error){
-            System.out.println(error);
-        }
-
-        return Integer.toString(sum);
-    }
-
-    public String[] validateInput(String[] strArray){
-        StringBuilder sb = new StringBuilder();
-        for (String value : strArray) {
-            if(!value.isEmpty() && Integer.parseInt(value) < 0){
-                sb.append(value);
-            }
-        }
-
-        if(!sb.toString().isEmpty()){
-            throw new NegativeNumberException(sb.toString());
-        }
-
-        return strArray;
     }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,49 +3,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
-    private StringCalculator stringCalculator;
-
-    @Before
-    public void setUp() {
-        stringCalculator = new StringCalculator();
-    }
-
-    @Test
-    public void testEmptyReturnsZero() {
-        String result = stringCalculator.add("");
-        assertEquals("0", result);
-    }
-
-    @Test
-    public void testSingleInputReturnsSameValue() {
-        String result = stringCalculator.add("3");
-        assertEquals("3", result);
-    }
-
-    @Test
-    public void testSumOf2Values() {
-        String result = stringCalculator.add("10,20");
-        assertEquals("30", result);
-    }
-
-    @Test
-    public void testSumOfNewLineSeparatedValues() {
-        String result = stringCalculator.add("1\n2");
-        assertEquals("3", result);
-    }
-
-    @Test
-    public void testSumOf3Values() {
-        String result = stringCalculator.add("10,20,40");
-        assertEquals("70", result);
-        String resultByNewline = stringCalculator.add("10\n20\n40");
-        assertEquals("70", resultByNewline);
-    }
-
-    @Test(expected=NegativeNumberException.class)
-    public void testNegativeNumbersException() throws Exception {
-        String result = stringCalculator.add("-1,2,-3");
-        assertEquals("0", result);
-    }
 
 }
